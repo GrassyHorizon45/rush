0a7ba09a85e94bb4b718840df5be4a51235e4fa1
GrassyHorizon45 <87006140+GrassyHorizon45@users.noreply.github.com>
2026-04-10 23:34:44 +0800

    Update rush01.c

--- ex00/rush01.c
+++ ex00/rush01.c
@@ -3,9 +3,25 @@
 #include <stdio.h>
 #include <unistd.h>
 
-void rush(void);
-
-void rush(void)
+void  ft_putchar(char  c);
+void  ft_nonexistent(void);
+//w:width, l:left, m:middle, r:right. i is just a variable
+void  draw(int  w, char  l, char  m, char  r)
 {
+  int i;
 
+  if (w <= 0)
+    return ;
+  ft_putchar(l);
+  i = 0;
+  while (
+
+void  rush(int x, int y)
+{
+  if (x <= 0 || y <= 0)
+  {
+    ft_nonexistent();
+    return ;
+  }
+  ft_draw(x, '/', '*'
 }
